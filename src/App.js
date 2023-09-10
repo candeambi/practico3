55ba1f4c030655d02160319767b3f3dc47be7c3a
candeambi <candeambi@gmail.com>
2023-09-10 18:20:45 -0300

    modifico App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Nombre from './componentes/Nombre.js';
 
 function App() {
+
+  const [nombre, setNombre] = useState('');
+
+  const ChangeNameApp = (nuevoNombre) => {
+    setNombre(nuevoNombre);
+  };
+
   return (
+    
     <div className="App">
       <header className="App-header">
+
+      <h1>Piedra, papel o tijera</h1>
+      
+      <Nombre nombre={nombre} ChangeName={ChangeNameApp}/>
+
+      <button type="submit" id="btnComenzar">Comenzar juego</button>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hola mundo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
