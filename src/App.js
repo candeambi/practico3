48137249266662775164eab4caaa84ef232d3783
candeambi <candeambi@gmail.com>
2023-09-16 15:17:40 -0300

    Borro toda la funcionalidad de App y vuelvo a intentar en InterfazUsuario

--- src/App.js
+++ src/App.js
@@ -17,77 +17,6 @@ function App() {
   //Controlador de eventos para iniciar el juego
   const [gameStarted, setGameStarted] = useState(false);
 
-  const opciones = ['Piedra', 'Papel', 'Tijera'];
-  const [opcionUsuario, setOpcionUsuario] = useState(null);
-  const [opcionComputadora, setOpcionComputadora] = useState(null);
-  const [resultado, setResultado] = useState(null);
-  const rondaMaxima = 5;
-  const [ronda, setRonda] = useState(1);
-  const [puntosUsuario, setPuntosUsuario] = useState(0);
-  const [puntosComputadora, setPuntosComputadora] = useState(0);
-
-  const obtenerOpcionClick = (opcion) => {
-    // Aquí puedes manejar la elección del jugador
-    if (ronda <= rondaMaxima){
-    console.log(`El jugador eligió: ${opcion}`);
-    setOpcionUsuario(opcion);
-    const eleccionAleatoria = opciones[Math.floor(Math.random() * 3)];
-    console.log(`La computadora eligió: ${eleccionAleatoria}`);
-    setOpcionComputadora(eleccionAleatoria);
- 
-    const resultadoRonda = obtenerResultado(opcion, eleccionAleatoria);
-    setResultado(resultadoRonda);
-
-    if (resultadoRonda === 'Usuario'){
-      setPuntosUsuario(puntosUsuario + 1);
-    } else if (resultadoRonda === 'Computadora') {
-      setPuntosComputadora(puntosComputadora + 1);
-    }
-    }
-
-    if (ronda >= rondaMaxima){
-      console.log('final');
-    }
-  };
-
-
-    const obtenerResultado = (opcionUsuario, opcionComputadora) =>{
-      opcionUsuario = opcionUsuario.toLowerCase();
-      opcionComputadora = opcionComputadora.toLowerCase();
-
-  
-      if (opcionUsuario === opcionComputadora){
-        console.log("empate");
-        return 'Empate';
-      } else if (
-      (opcionComputadora === "Piedra" && opcionUsuario === "Tijeras") ||
-      (opcionComputadora === "Papel" && opcionUsuario === "Piedra") ||
-      (opcionComputadora === "Tijeras" && opcionUsuario === "Papel")
-      ){
-          console.log("computadora");
-          setRonda(ronda +1);
-          return 'Computadora';
-      } else if (
-      (opcionUsuario === "Piedra" && opcionComputadora === "Tijeras") ||
-      (opcionUsuario === "Papel" && opcionComputadora === "Piedra") ||
-      (opcionUsuario === "Tijeras" && opcionComputadora === "Papel")
-      ){
-          console.log("usuario");
-          setRonda(ronda +1);
-          return 'Usuario';
-      }
-      
-     
-      //setResultado(obtenerResultado(opcionUsuario, opcionComputadora));
-      console.log(`Resultado de la ronda: ${resultado}`);
-
-      };
-  
-      useEffect(() => {
-    // Esto se ejecuta cada vez que resultado se actualiza
-    console.log(`Resultado de la ronda: ${resultado}`);
-  }, [resultado]); 
-  
   return (
     
     <div className="App">
@@ -105,12 +34,7 @@ function App() {
       ) : (
         // Mostrar botones de juego cuando gameStarted es true
         <div>
-          <InterfazUsuario opcionClick={obtenerOpcionClick}/>
-          <p>Elegiste: {opcionUsuario}</p>
-          <p>La computadora eligió: {opcionComputadora}</p>
-          {resultado !== null && (
-          <p>Resultado de la ronda: {resultado}</p>
-          )}
+          <InterfazUsuario />
           <Marcador />
         </div>
       )}
@@ -121,5 +45,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- src/componentes/InterfazUsuario.js
+++ src/componentes/InterfazUsuario.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import piedraImg from '../img/piedra.jpg';
 import papelImg from '../img/papel.jpg';
 import tijeraImg from '../img/tijera.jpg';
 
-function InterfazUsuario({ opcionClick }) {
+function InterfazUsuario() {
+
+
+  const [opcionUsuario, setOpcionUsuario] = useState(null);
+  const [opcionComputadora, setOpcionComputadora] = useState(null);
+  const [resultado, setResultado] = useState(null);
+
+  // Declaro la funcion determinarGanador
+  function determinarGanador(opcionUsuario, opcionComputadora){
+    if (opcionUsuario === opcionComputadora){
+      console.log('Empate');
+      setResultado('Empate');
+    }
+    else if (
+      (opcionUsuario === 'Piedra' && opcionComputadora === 'Tijera') ||
+      (opcionUsuario === 'Papel' && opcionComputadora === 'Piedra') ||
+      (opcionUsuario === 'Tijera' && opcionComputadora === 'Papel')
+    ){
+      console.log('Gana el usuario');
+      setResultado('Gana el usuario');
+    }
+    else if (
+      (opcionComputadora === 'Piedra' && opcionUsuario === 'Tijera') ||
+      (opcionComputadora === 'Papel' && opcionUsuario === 'Piedra') ||
+      (opcionComputadora === 'Tijera' && opcionUsuario === 'Papel')
+    ){
+      console.log('Gana la computadora');
+      setResultado('Gana la computadora');
+    };
+  }
+
+  //Guardo la opcion seleccionada por el jugador
+  const opcionClick = (opcion) => {
+  if (opcion === 'Piedra') setOpcionUsuario('Piedra');
+  if (opcion === 'Papel') setOpcionUsuario('Papel');
+  if (opcion === 'Tijera') setOpcionUsuario('Tijera');
+  console.log(`El jugador eligió: ${opcion}`);
+
+  //Realizo la eleccion aleatoria de la computadora
+  let opciones = ['Piedra', 'Papel', 'Tijera'];
+  let eleccionAleatoria = opciones[Math.floor(Math.random() * 3)];
+  if (eleccionAleatoria === 'Piedra') setOpcionComputadora('Piedra');
+  if (eleccionAleatoria === 'Papel') setOpcionComputadora('Papel');
+  if (eleccionAleatoria === 'Tijeras') setOpcionComputadora('Tijera');
+  console.log(`La computadora eligió: ${eleccionAleatoria}`);
+
+  //Realizo la funcion que determina el ganador
+  determinarGanador(opcionUsuario, opcionComputadora);
+  };
+  
+
+
+
     return (
       <div className="usuario-interfaz">
         <h2>Elige tu opción:</h2>
@@ -21,10 +73,14 @@ function InterfazUsuario({ opcionClick }) {
             <img src={tijeraImg} alt="tijera" />
             </button>
         </div>
-
+        <div>
+        <p>Elegiste: {opcionUsuario}</p>
+        <p>La computadora eligió: {opcionComputadora}</p>
+        <p>Resultado de la ronda: {resultado}</p>
         </div>
+      </div>
       </div>
     );
   }
-  
-  export default InterfazUsuario;
+
+    export default InterfazUsuario;
