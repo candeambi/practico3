9163e41b94897c011702d900976b4089f4bec35a
candeambi <candeambi@gmail.com>
2023-09-20 16:16:13 -0300

    Modifico InterfazUsuario: Avisa quien es el ganador de la partida

--- src/componentes/InterfazUsuario.js
+++ src/componentes/InterfazUsuario.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import piedraImg from '../img/piedra.jpg';
 import papelImg from '../img/papel.jpg';
 import tijeraImg from '../img/tijera.jpg';
@@ -9,9 +9,12 @@ function InterfazUsuario({actualizarMarcador, nombre}) {
   const [opcionUsuario, setOpcionUsuario] = useState("");
   const [opcionComputadora, setOpcionComputadora] = useState("");
   const [resultado, setResultado] = useState("");
-  const [ronda, setRonda] = useState(0);
-  const rondaMaxima = 5;
+  const [ronda, setRonda] = useState(1);
   const [botonDesactivado, setBotonDesactivado] = useState(false);
+  const [juegoActivo, setJuegoActivo] = useState(true);
+  const [ganadorFinal, setGanadorFinal] = useState('');
+  const [puntosUsuario, setPuntosUsuario] = useState(0);
+  const [puntosComputadora, setPuntosComputadora] = useState(0);
   
 
   // Declaro la funcion determinarGanador
@@ -30,42 +33,50 @@ function InterfazUsuario({actualizarMarcador, nombre}) {
       setResultado('Gana '+ nombre);
       setRonda(ronda + 1);
       actualizarMarcador('usuario');
+      setPuntosUsuario(puntosUsuario + 1);
     }
     else {
       console.log('Gana la computadora');
       setResultado('Gana la computadora');
       setRonda(ronda +1);
       actualizarMarcador('computadora');
+      setPuntosComputadora(puntosComputadora + 1);
     };
 
   }
 
   //Guardo la opcion seleccionada por el jugador
   const opcionClick = (opcion) => {
-    if (ronda < rondaMaxima){
-  if (opcion === 'Piedra') setOpcionUsuario('Piedra');
-  if (opcion === 'Papel') setOpcionUsuario('Papel');
-  if (opcion === 'Tijera') setOpcionUsuario('Tijera');
-  console.log(`El jugador eligió: ${opcion}`);
+    if (juegoActivo) {
+      if (opcion === 'Piedra') setOpcionUsuario('Piedra');
+      if (opcion === 'Papel') setOpcionUsuario('Papel');
+      if (opcion === 'Tijera') setOpcionUsuario('Tijera');
+      console.log(`El jugador eligió: ${opcion}`);
 
-  //Realizo la eleccion aleatoria de la computadora
-  let opciones = ['Piedra', 'Papel', 'Tijera'];
-  let eleccionAleatoria = opciones[Math.floor(Math.random() * 3)];
-  if (eleccionAleatoria === 'Piedra') setOpcionComputadora('Piedra');
-  if (eleccionAleatoria === 'Papel') setOpcionComputadora('Papel');
-  if (eleccionAleatoria === 'Tijera') setOpcionComputadora('Tijera');
-  console.log(`La computadora eligió: ${eleccionAleatoria}`);
+      //Realizo la eleccion aleatoria de la computadora
+      let opciones = ['Piedra', 'Papel', 'Tijera'];
+      let eleccionAleatoria = opciones[Math.floor(Math.random() * 3)];
+      if (eleccionAleatoria === 'Piedra') setOpcionComputadora('Piedra');
+      if (eleccionAleatoria === 'Papel') setOpcionComputadora('Papel');
+      if (eleccionAleatoria === 'Tijera') setOpcionComputadora('Tijera');
+      console.log(`La computadora eligió: ${eleccionAleatoria}`);
 
-  //Realizo la funcion que determina el ganador
-  determinarGanador(opcion, eleccionAleatoria);
+      //Realizo la funcion que determina el ganador
+      determinarGanador(opcion, eleccionAleatoria);
+    }
+  };
 
-  
-
-} else if (ronda >= rondaMaxima){
-  setBotonDesactivado(true);
-};
-}
-
+  useEffect(() => {
+    if (puntosUsuario >= 3 || puntosComputadora >= 3) {
+      setJuegoActivo(false);
+      setBotonDesactivado(true);
+      if (puntosUsuario >= 3) {
+        setGanadorFinal(`¡${nombre} ganaste!`);
+      } else {
+        setGanadorFinal('Ganó la computadora');
+      }
+    }
+  }, [puntosUsuario, puntosComputadora, nombre]);
   
 
     return (
@@ -74,7 +85,7 @@ function InterfazUsuario({actualizarMarcador, nombre}) {
         <div className="opciones">
           {/* Aquí van los botones de elección del jugador */}
           <div>
-            <p>El mejor de 5 rondas gana.</p>
+            <p>Quien gane 3 rondas, gana la partida.</p>
           <h2>¡A jugar!</h2>
           <button onClick={() => opcionClick('Piedra')} disabled={botonDesactivado}>
             <img src={piedraImg} alt="piedra" />
@@ -87,6 +98,7 @@ function InterfazUsuario({actualizarMarcador, nombre}) {
             </button>
         </div>
         <div>
+        <h2>{ganadorFinal}</h2>
         <p>Elegiste: {opcionUsuario}</p>
         <p>La computadora eligió: {opcionComputadora}</p>
         <p>Resultado de la ronda: {resultado}</p>
